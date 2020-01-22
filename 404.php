1d0ffae506a371a7e2ff01fafa501c864bb7ae87
PlusSickHead15 <42875789+PlusSickHead15@users.noreply.github.com>
2020-01-22 18:04:29 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-		<img src="/images/image/IMG-2126.jpg" >
+		<img src="/images/image/" >
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'chriss-theme' ); ?></h1>
