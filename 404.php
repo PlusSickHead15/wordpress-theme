91b3a51c285b6ddcd15416de1a43fe09bc834b6d
PlusSickHead15 <42875789+PlusSickHead15@users.noreply.github.com>
2020-01-22 17:57:45 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,14 +12,14 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+		<img src="/images/IMG-2126.jpg" style="width:128px;height:128px;">
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'chriss-theme' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'chriss-theme' ); ?></p>
+					<p><?php esc_html_e( 'It looks like nothing was found up in dis bish', 'chriss-theme' ); ?></p>
 
 					<?php
 					get_search_form();
