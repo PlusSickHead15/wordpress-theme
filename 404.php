469a48291d7bf2127f03144d0535570fa658340e
PlusSickHead15 <42875789+PlusSickHead15@users.noreply.github.com>
2020-01-22 19:44:01 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-		<img src="wp-content/themes/wordpress-theme/IMG-2126.jpg" style="width:128px;height:128px;">
+		<img src="/cgroce84/wordpress/wp-content/themes/Wordpress-Theme/image/IMG-2126.jpg" style="width:128px;height:128px;">
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'chriss-theme' ); ?></h1>
