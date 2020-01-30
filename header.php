d8f06f3781844a6fc7828508740625fbd99754aa
PlusSickHead15 <42875789+PlusSickHead15@users.noreply.github.com>
2020-01-30 10:26:54 -0500

    Update header.php

--- header.php
+++ header.php
@@ -53,6 +53,11 @@
 			) );
 			?>
 		</nav><!-- #site-navigation -->
+		<div id="site-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+        </a>
+    </div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
