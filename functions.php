f003353f10e684c62b9ba69f13860c2995734318
PlusSickHead15 <42875789+PlusSickHead15@users.noreply.github.com>
2020-01-31 08:42:37 -0500

    fdga

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @package Chris\'s_Theme
  */
 
-if ( ! function_exists( 'chriss_theme_setup' ) ) :
+if ( ! function_exists( 'chriss_killer_theme_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -15,14 +15,14 @@ if ( ! function_exists( 'chriss_theme_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function chriss_theme_setup() {
+	function chriss_killer_theme_setup() {
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on Chris\'s Theme, use a find and replace
 		 * to change 'chriss-theme' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'chriss-theme', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'chriss-killer-theme', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -44,7 +44,7 @@ if ( ! function_exists( 'chriss_theme_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'chriss-theme' ),
+			'menu-1' => esc_html__( 'Primary', 'chriss-killer-theme' ),
 		) );
 
 		/*
@@ -60,7 +60,7 @@ if ( ! function_exists( 'chriss_theme_setup' ) ) :
 		) );
 
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'chriss_theme_custom_background_args', array(
+		add_theme_support( 'custom-background', apply_filters( 'chriss_killer_theme_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
@@ -89,7 +89,7 @@ if ( ! function_exists( 'chriss_theme_setup' ) ) :
         ));
 	}
 endif;
-add_action( 'after_setup_theme', 'chriss_theme_setup', 'Chriss_killer_theme_custom_header_setup');
+add_action( 'after_setup_theme', 'chriss_killer_theme_setup', 'chriss_killer_theme_custom_header_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -102,9 +102,9 @@ function chriss_theme_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'chriss_theme_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'chriss_killer_theme_content_width', 640 );
 }
-add_action( 'after_setup_theme', 'chriss_theme_content_width', 0 );
+add_action( 'after_setup_theme', 'chriss_killer_theme_content_width', 0 );
 
 /**
  * Register widget area.
@@ -113,32 +113,32 @@ add_action( 'after_setup_theme', 'chriss_theme_content_width', 0 );
  */
 function chriss_theme_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'chriss-theme' ),
+		'name'          => esc_html__( 'Sidebar', 'chriss-killer-theme' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'chriss-theme' ),
+		'description'   => esc_html__( 'Add widgets here.', 'chriss-killer-theme' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 }
-add_action( 'widgets_init', 'chriss_theme_widgets_init' );
+add_action( 'widgets_init', 'chriss_killer_theme_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
 function chriss_theme_scripts() {
-	wp_enqueue_style( 'chriss-theme-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'chriss-killer-theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'chriss-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'chriss-killer-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'chriss-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'chriss-killer-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'chriss_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'chriss_killer_theme_scripts' );
 
 /**
  * Implement the Custom Header feature.
